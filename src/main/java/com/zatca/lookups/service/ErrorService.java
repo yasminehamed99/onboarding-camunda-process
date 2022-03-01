bc9e63343832b30cc1bc9d01fa4203adc1652c32
MAlMakhadmeh <MAlMakhadmeh@agiletz.com>
2022-03-01 17:19:04 +0200

    Fix Search endpoint

--- src/main/java/com/zatca/lookups/service/ErrorService.java
+++ src/main/java/com/zatca/lookups/service/ErrorService.java
@@ -6,8 +6,14 @@ import com.zatca.lookups.entity.ErrorMessage;
 import com.zatca.lookups.repository.ErrorRepo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,9 +28,63 @@ public class ErrorService {
 
     public List<ErrorDTO> findErrorByCodeAndMessage(String errorCode, String message) {
 
-        List<ErrorMessage> errorMessages = errorRepo.findByCodeContainsAndArabicMessageContainsAndMessageContains(errorCode, message, message);
 
-        return errorMessages.stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+        Specification<ErrorMessage> specifications = new Specification<ErrorMessage>() {
+            @Override
+            public Predicate toPredicate(Root<ErrorMessage> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> predicates = new ArrayList<>();
+                if (errorCode != null && !errorCode.isBlank())
+                    predicates.add(criteriaBuilder.and(criteriaBuilder.like(root.get("code"), "%" + errorCode + "%")));
+
+                if (message != null && !message.isBlank()) {
+                    if (isProbablyArabic(message)) {
+                        predicates.add(criteriaBuilder.and(criteriaBuilder.like(root.get("arabicMessage"), "%" + message + "%")));
+                    } else {
+                        predicates.add(criteriaBuilder.and(criteriaBuilder.like(root.get("message"), "%" + message + "%")));
+                    }
+                }
+
+                Predicate finalPredicate = null;
+                if (predicates.isEmpty()) {
+                    finalPredicate = criteriaBuilder.conjunction();
+                } else {
+                    for (Predicate p : predicates) {
+                        if(finalPredicate == null)
+                            finalPredicate = criteriaBuilder.and(p);
+                        else
+                            finalPredicate = criteriaBuilder.and(finalPredicate, p);
+                    }
+                }
+
+                return finalPredicate;
+            }
+
+            private boolean isProbablyArabic(String s) {
+                for (int i = 0; i < s.length();) {
+                    int c = s.codePointAt(i);
+                    if (c >= 0x0600 && c <= 0x06E0)
+                        return true;
+                    i += Character.charCount(c);
+                }
+                return false;
+            }
+        };
+
+        return errorRepo.findAll(specifications).stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+
+//        if ((message == null || message.isEmpty()) && (errorCode!= null && !errorCode.isEmpty())) {
+//            return errorRepo.findByCodeContains(errorCode).stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+//        } else if (isProbablyArabic(message) && (errorCode!= null && !errorCode.isEmpty())){
+//            return errorRepo.findByCodeContainsAndArabicMessageContains(errorCode, message).stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+//        } else if (!isProbablyArabic(message) && (errorCode!= null && !errorCode.isEmpty())){
+//            return errorRepo.findByCodeContainsAndMessageContains(errorCode, message).stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+//        } else {
+//            return errorRepo.findAll().stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
+//        }
+
+//        List<ErrorMessage> errorMessages = errorRepo.findByCodeContainsAndArabicMessageContainsAndMessageContains(errorCode, message, message);
+
+//        return errorMessages.stream().map(e -> new ErrorDTO(e.getCode(), e.getMessage(), e.getArabicMessage(), e.getType())).collect(Collectors.toList());
 
     }
 
