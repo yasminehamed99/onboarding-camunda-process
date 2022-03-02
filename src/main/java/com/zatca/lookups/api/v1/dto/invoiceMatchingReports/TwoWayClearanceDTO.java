9fd9856ca73eccbe4380cf2cab4e757fa4444427
MAlMakhadmeh <MAlMakhadmeh@agiletz.com>
2022-03-02 11:58:19 +0200

    Add endpoint to update status

--- src/main/java/com/zatca/lookups/api/v1/dto/invoiceMatchingReports/TwoWayClearanceDTO.java
+++ src/main/java/com/zatca/lookups/api/v1/dto/invoiceMatchingReports/TwoWayClearanceDTO.java
@@ -3,6 +3,7 @@ package com.zatca.lookups.api.v1.dto.invoiceMatchingReports;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class TwoWayClearanceDTO {
@@ -15,5 +16,9 @@ public class TwoWayClearanceDTO {
     private long maxPeriodOfMatchingReports;
     @Min(value = 0, message = "Max Historical Data For Matching Reports can't be less than zero")
     private long maxHistoricalDataForMatchingReports;
+    @NotBlank(message = "Max Period Of Matching Reports Time can't be null or empty")
+    private String maxPeriodOfMatchingReportsTime;
+    @NotBlank(message = "Max Historical Data For Matching Reports Time can't be null or empty")
+    private String maxHistoricalDataForMatchingReportsTime;
 
 }
