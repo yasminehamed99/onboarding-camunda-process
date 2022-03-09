cb982e08547968f6492d1a9a5deefaa024fcfb70
Nedal Alzaideh <nal-zaideh@deloitte.com>
2022-03-09 16:21:55 +0300

    Add latest properties as data

--- src/main/java/com/zatca/lookups/api/v1/dto/cms/Faqs.java
+++ src/main/java/com/zatca/lookups/api/v1/dto/cms/Faqs.java
@@ -1,16 +1,18 @@
 package com.zatca.lookups.api.v1.dto.cms;
 
+import com.zatca.lookups.api.v1.dto.annotations.DataProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Data
 public class Faqs {
     @NotNull(message = "arabicIntroduction field is required")
+    @DataProperty
     public String arabicIntroduction;
     @NotNull(message = "englishIntroduction field is required")
+    @DataProperty
     public String englishIntroduction;
     @NotNull(message = "questions field is required")
     public List<Question> questions;
