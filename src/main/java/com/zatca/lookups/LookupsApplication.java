65c8cee01156a45e518e8bddb53309c97929778b
Marwan Althaher <malthaher@agiletz.com>
2022-05-13 08:37:28 +0300

    load script file from class path

--- src/main/java/com/zatca/lookups/LookupsApplication.java
+++ src/main/java/com/zatca/lookups/LookupsApplication.java
@@ -1,10 +1,13 @@
 package com.zatca.lookups;
 
-import com.zatca.lookups.entity.ErrorMessage;
-import com.zatca.lookups.entity.Lookup;
-import com.zatca.lookups.repository.ErrorRepo;
-import com.zatca.lookups.repository.LookupRepo;
-import lombok.extern.slf4j.Slf4j;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
@@ -15,11 +18,12 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-import java.util.Optional;
+import com.zatca.lookups.entity.ErrorMessage;
+import com.zatca.lookups.entity.Lookup;
+import com.zatca.lookups.repository.ErrorRepo;
+import com.zatca.lookups.repository.LookupRepo;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @Slf4j
@@ -51,14 +55,18 @@ public class LookupsApplication {
 		List<ErrorMessage> errorMessages = errorRepo.findAll();
 		Optional<Lookup> root = lookupRepo.findByCode(adminConfigRootCode);
 		if (ObjectUtils.isEmpty(root)) {
-			File file = new File("/resources/data.sql");
-			String insertStatements= new String(Files.readAllBytes(file.toPath()));
+			
+			String insertStatements = IOUtils.toString(new InputStreamReader(
+					getClass().getClassLoader().getResourceAsStream("data.sql")));
+			
 			jdbcTemplate.execute(insertStatements);
 		}
 
 		if (ObjectUtils.isEmpty(errorMessages)) {
-			File file = new File("/resources/messagesData.sql");
-			String insertStatements= new String(Files.readAllBytes(file.toPath()));
+			
+			String insertStatements = IOUtils.toString(new InputStreamReader(
+					getClass().getClassLoader().getResourceAsStream("messagesData.sql")));
+			
 			jdbcTemplate.execute(insertStatements);
 		}
 
